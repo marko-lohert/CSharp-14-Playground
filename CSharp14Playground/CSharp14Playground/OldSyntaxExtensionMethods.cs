f9ab80501b323c9271293fa103cfeea87ffaed71
marko-lohert <lohert.marko@gmail.com>
2025-10-21 23:04:03 +0200

    Add PrintHeader() in PlaygroundUtils. Also add an extension method using old syntax for extension methods, as an example that will be used for comparison with the new syntax for extension members.

---
+++ CSharp14Playground/CSharp14Playground/OldSyntaxExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace CSharp14Playground;
+
+public static class OldSyntaxExtensionMethods
+{
+    public static string Multiply(this string str, int count)
+    {
+        if (string.IsNullOrEmpty(str) || count <= 0)
+            return string.Empty;
+        
+        StringBuilder sb = new();
+        for (int i = 0; i < count; i++)
+        {
+            sb.Append(str);
+        }
+        
+        return sb.ToString();
+    }
+}
---
+++ CSharp14Playground/CSharp14Playground/PlaygroundUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp14Playground;
+
+public static class PlaygroundUtils
+{
+    public static void PrintHeader(string title, string lineElement = "-")
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            return;
+
+        Console.WriteLine(lineElement.Multiply(title.Length));
+        Console.WriteLine(title);
+        Console.WriteLine(lineElement.Multiply(title.Length));
+    }
+}
