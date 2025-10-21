6c7edfb5a192870d72e12eec1e638d2dce9ec510
marko-lohert <lohert.marko@gmail.com>
2025-10-22 00:40:22 +0200

    Add a demo that showcases null-conditional assignment.

---
+++ CSharp14Playground/CSharp14Playground/NullConditionalAssignment/DemoNullConditionalAssignment.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharp14Playground.NullConditionalAssignment;
+
+public static class DemoNullConditionalAssignment
+{
+    public static void StartDemo()
+    {
+        PlaygroundUtils.PrintHeader("Demo Null-conditional assignment");
+
+        Customer newCustomer = null;
+
+        // Before C# 14
+        if (newCustomer != null)
+            newCustomer.FirstName = "John";
+
+        if (newCustomer is not null)
+            newCustomer.FirstName = "John";
+
+        if (newCustomer is { })
+            newCustomer.FirstName = "John";
+
+        // C# 14
+        newCustomer?.FirstName = "John";
+
+        Console.WriteLine($"New customer: {newCustomer?.FirstName}");
+
+        Customer[] customers = null; // Suggested additional test: customers = [];
+
+        // Before C# 14
+        if (customers != null)
+            customers[0].FirstName = "John";
+
+        // C# 14
+        customers?[0].FirstName = "John"; // Check that customers is not null.
+        customers?[0]?.FirstName = "John"; // Check both that customers is not null and that it contains an item at index 0.
+    }
+}
--- CSharp14Playground/CSharp14Playground/Program.cs
+++ CSharp14Playground/CSharp14Playground/Program.cs
@@ -2,11 +2,14 @@
 using CSharp14Playground.ExtensionMembers;
 using CSharp14Playground.fieldKeyword;
 using CSharp14Playground.nameofUnboundGenericTypes;
+using CSharp14Playground.NullConditionalAssignment;
 using CSharp14Playground.PartialEventsAndConstructors;
 
 PlaygroundUtils.PrintHeader("C# 14 Playground", "=");
 
 DemoExtensionMembers.StartDemo();
+
+DemoNullConditionalAssignment.StartDemo();
 
 DemoNameofUnboundGenericTypes.StartDemo();
 
