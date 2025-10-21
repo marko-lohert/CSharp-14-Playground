9ea749e2c2f0866ad7762993f40548bfae41619a
marko-lohert <lohert.marko@gmail.com>
2025-10-22 00:11:54 +0200

    Add a demo showcasing unbound generic types and nameof().

--- CSharp14Playground/CSharp14Playground/Program.cs
+++ CSharp14Playground/CSharp14Playground/Program.cs
@@ -1,10 +1,13 @@
 ﻿using CSharp14Playground;
 using CSharp14Playground.ExtensionMembers;
 using CSharp14Playground.fieldKeyword;
+using CSharp14Playground.nameofUnboundGenericTypes;
 
 PlaygroundUtils.PrintHeader("C# 14 Playground", "=");
 
 DemoExtensionMembers.StartDemo();
+
+DemoNameofUnboundGenericTypes.StartDemo();
 
 DemoFieldKeyword fieldKeywordDemo = new();
 fieldKeywordDemo.StartDemo();
---
+++ CSharp14Playground/CSharp14Playground/nameofUnboundGenericTypes/DemoNameofUnboundGenericTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp14Playground.nameofUnboundGenericTypes;
+
+public class DemoNameofUnboundGenericTypes
+{
+    public static void StartDemo()
+    {
+        PlaygroundUtils.PrintHeader("Demo: nameof unbound generic types");
+
+        List<Employee> employees = [];
+
+        // Before C# 14
+        Console.WriteLine($"nameof: {nameof(List<Employee>)}");
+
+        // C# 14
+        Console.WriteLine($"nameof: {nameof(List<>)}");
+    }
+}
