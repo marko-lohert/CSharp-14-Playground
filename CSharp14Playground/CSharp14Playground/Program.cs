67f92b233706844f06752efd8e5e85e50138a1a6
marko-lohert <lohert.marko@gmail.com>
2025-10-22 00:03:05 +0200

    Add a demo showcasing the field keyword.

--- CSharp14Playground/CSharp14Playground/Program.cs
+++ CSharp14Playground/CSharp14Playground/Program.cs
@@ -1,6 +1,10 @@
 ﻿using CSharp14Playground;
 using CSharp14Playground.ExtensionMembers;
+using CSharp14Playground.fieldKeyword;
 
 PlaygroundUtils.PrintHeader("C# 14 Playground", "=");
 
 DemoExtensionMembers.StartDemo();
+
+DemoFieldKeyword fieldKeywordDemo = new();
+fieldKeywordDemo.StartDemo();
---
+++ CSharp14Playground/CSharp14Playground/fieldKeyword/DemoFieldKeyword.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharp14Playground.fieldKeyword;
+
+public class DemoFieldKeyword
+{
+    public void StartDemo()
+    {
+        PlaygroundUtils.PrintHeader("Demo: field keyword");
+
+        TempCelsius = -300;
+        Console.WriteLine($"Temp: {TempCelsius} °C");
+    }
+
+    public double TempCelsius
+    {
+        get => field;
+        set
+        {
+            if (value < AbsMinTemp)
+                field = -273.15;
+            else if (value > PlanckTemperature)
+                field = PlanckTemperature;
+            else
+                field = value;
+        }
+    }
+
+    private const double AbsMinTemp = -273.15;
+    private const double PlanckTemperature = 1.41E32;
+}
